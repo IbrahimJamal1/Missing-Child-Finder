8bffc9252ad2e0cf6d670e5bc3ef12c7721e776c
Ibrahim Jamal <ibrahimgamal932@gmail.com>
2026-09-12 14:54:44 +0300

    ios maps

--- Mobile-App/missingchild/ios/Runner/AppDelegate.swift
+++ Mobile-App/missingchild/ios/Runner/AppDelegate.swift
@@ -1,16 +1,28 @@
 import Flutter
 import UIKit
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+
+        GMSServices.provideAPIKey("AIzaSyDJp7iRjyEXrd6FdytSI7gT90LyFtK4Ryc")
+
+        return super.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+    }
+
+    func didInitializeImplicitFlutterEngine(
+        _ engineBridge: FlutterImplicitEngineBridge
+    ) {
+        GeneratedPluginRegistrant.register(
+            with: engineBridge.pluginRegistry
+        )
+    }
 }
--- Mobile-App/missingchild/lib/features/detailspost/widget/imagechaild.dart
+++ Mobile-App/missingchild/lib/features/detailspost/widget/imagechaild.dart
@@ -1,3 +1,4 @@
+import 'package:ai_safetrack/core/services/formatreportdate.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,7 +11,7 @@ Widget imagechaild(context, dynamic report) {
     leading: Padding(
       padding: EdgeInsets.all(8.r),
       child: CircleAvatar(
-        backgroundColor: Colors.black.withOpacity(0.4),
+        backgroundColor: Colors.black.withValues(alpha: 0.4),
         child: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () {
@@ -18,6 +19,44 @@ Widget imagechaild(context, dynamic report) {
           },
         ),
       ),
+    ),
+
+    title: Row(
+      children: [
+        Container(
+          width: 40.w,
+          height: 40.h,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: NetworkImage(report.reporterImage),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        SizedBox(width: 10.w),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              report.reporterName,
+              style: TextStyle(
+                fontSize: 20.sp,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 8.h),
+            Text(
+              formatReportDate(report.reportDate.toString()),
+              style: TextStyle(
+                fontSize: 14.sp,
+                color: Colors.white.withValues(alpha: 0.7),
+              ),
+            ),
+          ],
+        ),
+      ],
     ),
 
     flexibleSpace: FlexibleSpaceBar(
@@ -39,7 +78,10 @@ Widget imagechaild(context, dynamic report) {
                 gradient: LinearGradient(
                   begin: Alignment.topCenter,
                   end: Alignment.bottomCenter,
-                  colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+                  colors: [
+                    Colors.transparent,
+                    Colors.black.withValues(alpha: 0.7),
+                  ],
                 ),
               ),
             ),
